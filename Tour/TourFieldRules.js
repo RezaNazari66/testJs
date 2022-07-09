05884d2ff9f805f9337788bb58472aa2f38e93b3
Reza <nazari.reza66@gmail.com>
2022-07-09 21:18:05 +0430

    add tour js

---
+++ Tour/TourFieldRules.js
@@ -0,0 +1,147 @@
+﻿var passenger = $PASSENGER$;
+var segment = $SEGMENT$;
+var messageArray = new Array();
+var detectPersianLetterRegex = new RegExp("^[آ-ی' ']*$");
+var detectLatinRegex = new RegExp("^[a-zA-Z' ']*$");
+var detectNationalNumberRegex = new RegExp('^[0-9]{10}$');
+function checkRequiredFields() {
+    if (segment == 'NationalNumber') {
+        checkIranian(passenger);
+    } else if (segment == 'Passport') {
+        checkForeign(passenger);
+    }
+    if (messageArray.length != 0) {
+        return { type: 'COMPONENTS', labels: messageArray };
+    } else {
+        return null;
+    }
+}
+function checkIranian(passenger) {
+    if (!passenger['namePersian'] || !passenger['namePersian'].trim()) {
+        messageArray.push({ text: 'نام فارسی اجباری است', id: 'NamePersian' });
+    }
+    if (!detectPersianLetterRegex.test(passenger['namePersian'])) {
+        messageArray.push({ text: 'نام فارسی باید حروف فارسی باشد', id: 'NamePersian' });
+    }
+    if (passenger['namePersian'].length > 32) {
+        messageArray.push({ text: 'نام فارسی نمیتواند بیشتر از ۳۲ کاراکتر باشد.', id: 'NamePersian' });
+    }
+    if (!passenger['lastNamePersian'] || !passenger['lastNamePersian'].trim()) {
+        messageArray.push({ text: 'نام خانوادگی فارسی اجباری است', id: 'LastNamePersian' });
+    }
+    if (!detectPersianLetterRegex.test(passenger['lastNamePersian'])) {
+        messageArray.push({ text: 'نام خانوادگی فارسی باید حروف فارسی باشد', id: 'LastNamePersian' });
+    }
+    if (passenger['lastNamePersian'].length > 32) {
+        messageArray.push({ text: 'نام خانوادگی فارسی نمیتواند بیشتر از ۳۲ کاراکتر باشد.', id: 'LastNamePersian' });
+    }
+    if (!passenger['name'] || !passenger['name'].trim()) {
+        messageArray.push({ text: 'نام لاتین اجباری است', id: 'Name' });
+    }
+    if (!detectLatinRegex.test(passenger['name'])) {
+        messageArray.push({ text: 'نام لاتین باید حروف لاتین باشد', id: 'Name' });
+    }
+    if (passenger['name'].length > 32) {
+        messageArray.push({ text: 'نام لاتین نمیتواند بیشتر از ۳۲ کاراکتر باشد.', id: 'Name' });
+    }
+    if (!passenger['lastName'] || !passenger['lastName'].trim()) {
+        messageArray.push({ text: 'نام خانوادگی لاتین اجباری است', id: 'LastName' });
+    }
+    if (!detectLatinRegex.test(passenger['lastName'])) {
+        messageArray.push({ text: 'نام خانوادگی لاتین باید حروف لاتین باشد', id: 'LastName' });
+    }
+    if (passenger['lastName'].length > 32) {
+        messageArray.push({ text: 'نام خانوادگی لاتین نمیتواند بیشتر از ۳۲ کاراکتر باشد.', id: 'LastName' });
+    }
+    if (!passenger['birthDate']) {
+        messageArray.push({ text: 'تاریخ تولد اجباری است', id: 'BirthDate' });
+    }
+    if (isInFuture(passenger['birthDate'])) {
+        messageArray.push({ text: 'این تاریخ قابل انتخاب نیست', id: 'BirthDate' });
+    }
+    if (!passenger['gender']) {
+        messageArray.push({ text: 'جنسیت اجباری است', id: 'Gender' });
+    }
+    var nationalNumberIdentification = findIdentification(passenger, 'NationalNumber');
+    checkNationalNumberRequiredFields(nationalNumberIdentification, passenger);
+}
+function checkForeign(passenger) {
+    if (!passenger['name'] || !passenger['name'].trim()) {
+        messageArray.push({ text: 'نام لاتین اجباری است', id: 'Name' });
+    }
+    if (!passenger['lastName'] || !passenger['lastName'].trim()) {
+        messageArray.push({ text: 'نام خانوادگی لاتین اجباری است', id: 'LastName' });
+    }
+    if (!detectLatinRegex.test(passenger['name'])) {
+        messageArray.push({ text: 'نام لاتین باید حروف لاتین باشد', id: 'Name' });
+    }
+    if (passenger['name'].length > 32) {
+        messageArray.push({ text: 'نام لاتین نمیتواند بیشتر از ۳۲ کاراکتر باشد.', id: 'Name' });
+    }
+    if (!detectLatinRegex.test(passenger['lastName'])) {
+        messageArray.push({ text: 'نام خانوادگی لاتین باید حروف لاتین باشد', id: 'LastName' });
+    }
+    if (passenger['lastName'].length > 32) {
+        messageArray.push({ text: 'نام خانوادگی لاتین نمیتواند بیشتر از ۳۲ کاراکتر باشد.', id: 'LastName' });
+    }
+    if (!passenger['birthDate']) {
+        messageArray.push({ text: 'تاریخ تولد اجباری است', id: 'BirthDate' });
+    }
+    if (isInFuture(passenger['birthDate'])) {
+        messageArray.push({ text: 'فرمت تاریخ تولد صحیح نیست', id: 'BirthDate' });
+    }
+    if (!passenger['gender']) {
+        messageArray.push({ text: 'جنسیت اجباری است', id: 'Gender' });
+    }
+    var passportIdentification = findIdentification(passenger, 'Passport');
+    checkPassportRequiredFields(passportIdentification, passenger);
+}
+
+function findIdentification(passenger, type) {
+    if (passenger.identifications) {
+        return passenger.identifications.find((element) => element.identificationType == type);
+    } else {
+        return null;
+    }
+}
+function checkNationalNumberRequiredFields(nationalNumberIdentification) {
+    if (!nationalNumberIdentification.code || !nationalNumberIdentification.code.trim()) {
+        messageArray.push({ text: 'کد ملی اجباری است', id: 'NationalNumber' });
+        return messageArray;
+    } else if (!detectNationalNumberRegex.test(nationalNumberIdentification.code)) {
+        messageArray.push({ text: 'کد ملی نامعتبر است', id: 'NationalNumber' });
+        return messageArray;
+    }
+}
+function checkPassportRequiredFields(passportIdentification, passenger) {
+    if (!passportIdentification.placeOfBirth) {
+        messageArray.push({ text: 'کشور محل تولد اجباری است', id: 'PlaceOfBirth' });
+    }
+    if (!passportIdentification.expiryDate) {
+        messageArray.push({ text: 'تاریخ انقضای پاسپورت اجباری است', id: 'ExpiryDate' });
+    }
+    if (!isInFuture(passportIdentification.expiryDate)) {
+        messageArray.push({ text: 'این تاریخ قابل انتخاب نیست', id: 'ExpiryDate' });
+    }
+    if (dateDiffInDays(passportIdentification.expiryDate) < 180) {
+        messageArray.push({ text: 'تاریخ انقضای پاسپورت باید بیشتر از ۶ ماه باشد', id: 'ExpiryDate' });
+    }
+    if (!passportIdentification.code || !passportIdentification.code.trim()) {
+        messageArray.push({ text: 'شماره پاسپورت اجباری است', id: 'PassportNumber' });
+    }
+    if (!passportIdentification.placeOfIssue) {
+        messageArray.push({ text: 'محل صدور پاسپورت اجباری است', id: 'PlaceOfIssue' });
+    }
+    return messageArray;
+}
+function isInFuture(date) {
+    return new Date(date) > new Date();
+}
+function dateDiffInDays(date) {
+    var currentTime = new Date();
+    var secondDate = new Date(date);
+    var diffTime = Math.abs(currentTime - secondDate);
+    var diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    return diffDays;
+}
+checkRequiredFields();
---
+++ Tour/TourSelectRules.js
@@ -0,0 +1,162 @@
+﻿var searchedObject = $SEARCHED_OBJECT$;
+var ticket = $TICKET$;
+var passengers = $PASSENGERS$;
+var selectedPassengersIds = $SELECTED_PASSENGERS_IDS$;
+var selectedPassengerIdsInOthersRoom = $SELECTED_PASSENGERS_IDS_IN_OTHERS_ROOM$;
+var isOnSubmit = $IS_ON_SUBMIT$;
+var items = new Array();
+var alert = null;
+function getAlertMessage(message, alertType, title, actions) {
+    return { type: alertType, title: title, message: message, actions: actions };
+}
+function getEditItemMessage(message, id, section, isEnabled, isSelected) {
+    return { id: id, isEnabled: isEnabled, isSelected: isSelected, section: section, message: message };
+}
+function showFinalResult() {
+    if (alert || items.length > 0) {
+        return { alert: alert, items: items };
+    } else {
+        return { alert: null, items: null };
+    }
+}
+function validatePassengersPax(list) {
+    var adultCountInSearch = searchedObject.adult;
+    var childCountInSearch = searchedObject.child;
+    var infantCountInSearch = searchedObject.infant;
+    list.forEach((passenger) => {
+        if (!passenger.birthDate || !passenger.gender || !passenger.lastName || !passenger.name) {
+            items.find((item) => item.id === passenger.id)['message'] = 'اطلاعات مسافر برای خرید بلیط تور نیاز به ویرایش دارد.';
+            return;
+        }
+        if (items.find((item) => item.id === passenger.id)["section"] == "INVALID") {
+            return;
+        }
+        var passportIdentification = findIdentification(passenger, 'Passport');
+        var nationalNumberIdentification = findIdentification(passenger, 'NationalNumber');
+        var message = '';
+        if ((nationalNumberIdentification && nationalNumberIdentification.code) || !passportIdentification || !passportIdentification.code) {
+            message = validateNationalNumberIdentification(passenger, nationalNumberIdentification);
+        } else if (passportIdentification) {
+            message = validatePassportIdentification(passenger, passportIdentification);
+        }
+        items.find((item) => item.id === passenger.id)['message'] = message;
+    });
+    var adultCount = list.filter((x) => isAdult(x.birthDate)).length;
+    var childCount = list.filter((x) => isChild(x.birthDate)).length;
+    var infantCount = list.filter((x) => isInfant(x.birthDate)).length;
+    if (list.length > 0) {
+        if (adultCount > adultCountInSearch) {
+            var lastId = list.pop().id;
+            items.find((item) => item.id === lastId)['isSelected'] = false;
+            alert = getAlertMessage('تعداد بزرگسالان انتخابی بیشتر از درخواست شما می باشد.', 'INFO', '');
+            return;
+        } else if (childCount > childCountInSearch) {
+            var lastId = list.pop().id;
+            items.find((item) => item.id === lastId)['isSelected'] = false;
+            alert = getAlertMessage('تعداد کودکان انتخابی بیشتر از درخواست شما می باشد.', 'INFO', '');
+            return;
+        } else if (infantCount > infantCountInSearch) {
+            var lastId = list.pop().id;
+            items.find((item) => item.id === lastId)['isSelected'] = false;
+            alert = getAlertMessage('تعداد نوزادان انتخابی بیشتر از درخواست شما می باشد.', 'INFO', '');
+            return;
+        } else if (adultCount == 0) {
+            alert = getAlertMessage('تعداد بزرگسالان نمی‌تواند کمتر از ۱ باشد ', 'INFO', '');
+            return;
+        } else if (infantCount > adultCount) {
+            alert = getAlertMessage('تعداد نوزادها نمی‌تواند بیشتر از تعداد بزرگسالان باشد ', 'INFO', '');
+            return;
+        } else if (adultCount + childCount + infantCount > 9) {
+            var lastId = list.pop().id;
+            items.find((item) => item.id === lastId)['isSelected'] = false;
+            alert = getAlertMessage('حداکثر تعداد مسافران برای پروازهای داخلی ۹ نفر است ', 'INFO', '');
+            return;
+        } else if (3 * adultCount < childCount || 3 * adultCount < childCount + infantCount) {
+            alert = getAlertMessage('به ازای هر بزرگسال، سه کودک، یا دو کودک و یک نوزاد مجاز است ', 'INFO', '');
+            return;
+        }
+    }
+    if (isOnSubmit == true) {
+        var hasError = items.find((item) => item.message && item.isSelected);
+        var searchPaxCount = adultCountInSearch + childCountInSearch + infantCountInSearch;
+        var paxCount = list.length;
+        if (hasError) {
+            var title = 'نقص اطلاعات در مسافرین';
+            alert = getAlertMessage('اطلاعات مسافرین انتخابی نیاز به ویرایش دارد.', 'INFO', title);
+        } else if (paxCount != searchPaxCount) {
+            var title = 'ادامه با '.concat(paxCount).concat(' مسافر');
+            var actions = [
+                { title: title, link: 'self://in-app/action?value=continue', color: '#554433' },
+                { title: 'بازگشت', link: 'self://in-app/action?value=cancel', color: '#554433' },
+            ];
+            alert = getAlertMessage('تعداد مسافرین انتخابی با درخواست شما مطابقت ندارد.', 'INFO', title, actions);
+        }
+    }
+}
+function validatePassenger(passenger) {
+    var message = '';
+    if (searchedObject.child == 0 && isChild(passenger.birthDate)) {
+        items.push(getEditItemMessage('کودک انتخابی با درخواست شما مطابقت ندارد', passenger.id, 'INVALID', false, false));
+    } else if (searchedObject.infant == 0 && isInfant(passenger.birthDate)) {
+        items.push(getEditItemMessage('نوزاد انتخابی با درخواست شما مطابقت ندارد', passenger.id, 'INVALID', false, false));
+    } else if (selectedPassengerIdsInOthersRoom != null && selectedPassengerIdsInOthersRoom.find(selected => selected == passenger.id)) {
+        items.push(getEditItemMessage('این مسافر قبلا در اتاقی دیگر انتخاب شده', passenger.id, 'INVALID', false, false));
+        passenger['isSelected'] = false;
+    } else {
+        var isSelected = passenger.isSelected || false;
+        items.push(getEditItemMessage(message, passenger.id, 'MAIN', true, isSelected));
+    }
+}
+function isAdult(birthDate) {
+    return dateDiffInDays(birthDate) > 12 * 365;
+}
+function isChild(birthDate) {
+    return dateDiffInDays(birthDate) > 2 * 365 && dateDiffInDays(birthDate) < 12 * 365;
+}
+function isInfant(birthDate) {
+    return dateDiffInDays(birthDate) > 0 && dateDiffInDays(birthDate) < 2 * 365;
+}
+function validatePassportIdentification(passenger, identification) {
+    if (identification.placeOfIssue == 'IRN' || identification.placeOfIssue == 'Iran') {
+        alert = getAlertMessage('اطلاعات مسافر را از قسمت مسافر ایرانی تکمیل نمایید.', 'INFO', '');
+        items.find((item) => item.id === passenger.id)['isSelected'] = false;
+    }
+    var passwordExpirationDiffrenceInDays = dateDiffInDays(identification.expiryDate);
+    if (!passenger.name.trim() || !passenger.lastName.trim() || passwordExpirationDiffrenceInDays < 180 || !identification.placeOfBirth || !identification.expiryDate || !identification.code || !identification.placeOfIssue) {
+        return 'اطلاعات مسافر برای خرید بلیط تور نیاز به ویرایش دارد.';
+    }
+}
+function validateNationalNumberIdentification(passenger, identification) {
+    if (!passenger.namePersian.trim() || !passenger.lastNamePersian.trim() || !identification.code) {
+        return 'اطلاعات مسافر برای خرید بلیط تور نیاز به ویرایش دارد.';
+    }
+}
+function findIdentification(passenger, type) {
+    if (passenger.identifications != null && passenger.identifications.length > 0) {
+        var idObject = null;
+        passenger.identifications.forEach((element) => {
+            if (element.identificationType == type) {
+                idObject = element;
+            }
+        });
+        return idObject;
+    } else {
+        return null;
+    }
+}
+function dateDiffInDays(date) {
+    var currentTime = new Date();
+    var secondDate = new Date(date);
+    var diffTime = Math.abs(currentTime - secondDate);
+    var diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    return diffDays;
+}
+var selectedPassengers = new Array();
+selectedPassengersIds.forEach((id) => {
+    var item = passengers.find((passenger) => passenger.id === id);
+    selectedPassengers.push(item);
+    passengers.find((passenger) => passenger.id === id)['isSelected'] = true;
+});
+passengers.forEach(validatePassenger);
+validatePassengersPax(selectedPassengers);
+showFinalResult();
